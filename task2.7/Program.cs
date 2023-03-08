a6caa80b5a2280083599ef65d29a6057ae8251ed
natlya_voevodina <voevodochka1988@yandex.ru>
2023-03-08 17:05:59 +0300

    task2.7

---
+++ task2.7/Program.cs
@@ -0,0 +1,68 @@
+﻿//Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
+
+//Например, задан массив:
+
+//1 4 7 2
+
+//5 9 2 3
+
+//8 4 2 4
+
+//17 -> такого числа в массиве нет
+
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-100, 100);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+
+string ArraySerch(int[,] array, int num)
+{
+    string result = $"{num} - Такого элемента в массиве нет!";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if ((array[i, j] == num))
+            {
+                result = $"индекс введенного элемента -> {i}, {j}";
+                
+            }
+        }
+    }
+    return result;
+}
+
+
+Console.WriteLine("Введите значение элемента");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+int num = number;
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(ArraySerch (array,num));
+
+
+
+
+
+
+
