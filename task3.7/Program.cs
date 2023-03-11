fd9c17552ec2dcb23e8653b6257c75e359258eff
natlya_voevodina <voevodochka1988@yandex.ru>
2023-03-11 06:13:35 +0300

    task3.7

--- task3.7/Program.cs
+++ task3.7/Program.cs
@@ -12,7 +12,7 @@ void PrintArray(int[,] array)
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write($"{array[i, j],3}  \t");
+            Console.Write($"{array[i, j],3} \t");
         Console.WriteLine();
     }
 }
